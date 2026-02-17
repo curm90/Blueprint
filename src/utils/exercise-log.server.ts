7a487df51ac7a9c48ec49b344c0d7ff466a64bd4
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2026-02-17 14:09:20 +0000

    feat: update addSessionLogServer to simplify session log creation by removing exerciseId parameter

--- src/utils/exercise-log.server.ts
+++ src/utils/exercise-log.server.ts
@@ -5,5 +5,5 @@ import { sessionLogCreateSchema } from '@/db/schema'
 export const addSessionLogServer = createServerFn({ method: 'POST' })
   .inputValidator((data: unknown) => sessionLogCreateSchema.parse(data))
   .handler(async ({ data }) => {
-    return await addSessionLog(data.exerciseId, data)
+    return await addSessionLog(data)
   })
