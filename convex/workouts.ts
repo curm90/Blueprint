b35fd99051958cef391478282e0cb4971287dc10
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2026-04-05 19:01:41 +0100

    feat: refactor addWorkout mutation to include selectedDays, weightUnit, and exercises

--- convex/workouts.ts
+++ convex/workouts.ts
@@ -4,20 +4,27 @@ import { mutation, query } from './_generated/server'
 export const addWorkout = mutation({
   args: {
     title: v.string(),
-    weight: v.number(),
-    minReps: v.number(),
-    maxReps: v.number(),
+    selectedDays: v.array(v.string()),
+    weightUnit: v.string(),
+    exercises: v.array(
+      v.object({
+        exerciseTitle: v.string(),
+        weight: v.number(),
+        minReps: v.number(),
+        maxReps: v.number(),
+      }),
+    ),
   },
 
   handler: async (ctx, args) => {
-    const { title, weight, minReps, maxReps } = args
+    const { title, selectedDays, weightUnit, exercises } = args
     console.log({ args })
 
     const id = await ctx.db.insert('workouts', {
       title,
-      weight,
-      minReps,
-      maxReps,
+      selectedDays,
+      weightUnit,
+      exercises,
     })
 
     console.log({ id })
