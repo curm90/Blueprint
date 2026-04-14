03ed3a5b0551c84b95bc44fde04b33e0f2628c66
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2026-04-14 12:29:31 +0100

    feat: adjust FooterNav padding for improved mobile responsiveness

--- src/components/FooterNav.tsx
+++ src/components/FooterNav.tsx
@@ -3,7 +3,7 @@ import FooterNavLink from './FooterNavLink'
 
 export default function FooterNav() {
   return (
-    <nav className='flex justify-between px-14 py-4 border border-top fixed bottom-0 left-0 right-0 bg-background z-10 sm:hidden'>
+    <nav className='flex justify-between px-8 py-4 border border-top fixed bottom-0 left-0 right-0 bg-background z-10 sm:hidden'>
       <ul className='flex justify-between items-center w-full'>
         {links.map((link) => (
           <FooterNavLink key={link.to} {...link} />
